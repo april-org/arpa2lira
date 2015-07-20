1cf2a55fa61b53384191a57cc1b91e6f5407729e
Paco Zamora Martinez <pakozm@gmail.com>
2015-07-20 12:46:10 +0200

    Added new files

---
+++ .dir-locals.el
@@ -0,0 +1,25 @@
+(
+ (nil .
+      (
+       (c-basic-offset . 2)
+       (tab-width . 8)
+       (indent-tabs-mode . nil)
+       )
+      )
+ (c-mode .
+         (
+          (flycheck-clang-include-path
+           "/usr/include/april-ann"
+           "/usr/include/lua5.2/"
+           )
+          )
+         )
+ (c++-mode .
+           (
+            (flycheck-clang-include-path
+             "/usr/include/april-ann"
+             "/usr/include/lua5.2/"
+             )
+            )
+           )
+ )
